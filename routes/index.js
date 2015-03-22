5d7fd5a1c7710f7835fa8424c4c5c6f1ee9bb192
Simon Twiforce <twiforce@yandex.ru>
2015-03-22 20:53:50 +0300

    Index page template

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    res.render('index', {title: 'suzume', subtitle: 'Node.JS danbooru-style imageboard'});
 });
 
 module.exports = router;
